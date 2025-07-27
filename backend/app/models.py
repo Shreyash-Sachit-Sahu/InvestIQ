ede47988fa2b22ce2a36b4f79e50d084893446e6
Shreyash-Sachit-Sahu <137154025+Shreyash-Sachit-Sahu@users.noreply.github.com>
2025-07-28 01:36:50 +0530

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,7 +5,7 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False, index=True)
+    email = db.Column(db.String(240), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(256), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
